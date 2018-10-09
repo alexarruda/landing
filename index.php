de72f20bb3ca1f48bb74e3a783ce56f13f766eed
alexarruda <alexsandroweslei@outlook.com>
2018-10-09 16:44:20 -0300

    add arquivos iniciais

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gerencia Leads</title>
+	<link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+	<section class="home center">
+		<div class="home-link">
+			<ul>
+				<li><a href="#">facebook</a></li>
+				<li><a href="#">instagram</a></li>
+				<li><a href="#">site oficial</a></li>
+			</ul>
+		</div>
+		<div class="home-side">
+			<div class="side-top">
+				<h1>agrobrazil</h1>
+			</div>
+			<div class="side-half">
+				<div><p>informacoes reais</p></div>
+				<div><p>geram bons resultados.</p></div>
+			</div>
+			<div class="side-down">
+				
+			</div>
+		</div>
+		<div class="home-whatsapp">
+			<a href="#">whatsapp</a>
+		</div>
+		<div class="home-more">
+			<a href="#">SAIBA COMO</a>
+		</div>
+	</section>
+	<section class="premium center">
+		<div class="premium-left">
+			
+		</div>
+		<div class="premium-right">
+			
+		</div>
+	</section>
+</body>
+</html>
