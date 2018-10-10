99f25d3e1504faf389d7b32d56049bca2c228347
Alex Arruda <alexsandroweslei@outlook.com>
2018-10-09 23:31:16 -0300

    add img e iniciado css

--- index.php
+++ index.php
@@ -2,10 +2,22 @@
 <html>
 <head>
 	<title>Gerencia Leads</title>
+	<link rel="stylesheet" href="fonts/glober.css">
 	<link rel="stylesheet" href="css/main.css">
 </head>
 <body>
-	<section class="home center">
+	<section class="home">
+		<div class="home-side">
+			<div class="side-top side-unit">
+				<img src="img/agrobrazil.png" alt="AgroBrazil"/>
+			</div>
+			<div class="side-half">
+				<div class="half half-1"><p>informações reais</p></div>
+				<div class="half half-2"><p>geram bons resultados.</p></div>
+			</div>
+			<div class="side-down side-unit"></div>
+		</div>
+
 		<div class="home-link">
 			<ul>
 				<li><a href="#">facebook</a></li>
@@ -13,31 +25,37 @@
 				<li><a href="#">site oficial</a></li>
 			</ul>
 		</div>
-		<div class="home-side">
-			<div class="side-top">
-				<h1>agrobrazil</h1>
+
+		<div class="home-bottom">
+			<div class="home-more">
+				<a href="#">
+					<div class="home-icon">V</div><br>
+					<span class="home-saiba">SAIBA COMO</span></a>
 			</div>
-			<div class="side-half">
-				<div><p>informacoes reais</p></div>
-				<div><p>geram bons resultados.</p></div>
+			<div class="home-whatsapp">
+				<a href="#"><img src="img/whats.png" alt="WhatsApp AgroBrazil"/></a>
 			</div>
-			<div class="side-down">
+		</div>
+	</section>
+
+	<section class="premium">
+		<div class="center">
+			<div class="premium-left">
+				
+			</div>
+			<div class="premium-right">
 				
 			</div>
 		</div>
-		<div class="home-whatsapp">
-			<a href="#">whatsapp</a>
-		</div>
-		<div class="home-more">
-			<a href="#">SAIBA COMO</a>
-		</div>
 	</section>
-	<section class="premium center">
-		<div class="premium-left">
-			
-		</div>
-		<div class="premium-right">
-			
+	<section class="form">
+		<div class="center">
+			<div class="premium-left">
+				
+			</div>
+			<div class="premium-right">
+				
+			</div>
 		</div>
 	</section>
 </body>
